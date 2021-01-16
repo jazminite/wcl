005f588b24abcb6a0b328be503750fe0b55c16ad
Jazminite <jazminitehs@gmail.com>
2021-01-16 12:54:10 -0500

    update warrior.py

--- warrior.py
+++ warrior.py
@@ -22,20 +22,20 @@ service = build('sheets', 'v4', http=credentials.authorize(Http()))
 
 wb = gc.open_by_key(secrets.google_sheet_id)
 
-def get_debuffs(reports, table, encounters, abilities, sheet_info):
+def get_effective(reports, table, encounters, abilities, expression, sheet_info):
   for report in reports:
     reportId = str(report['id'], 'utf-8')
     print(report['date'], report['title'])
     for encounter in encounters:
       for ability in abilities:
-        table_url = 'https://classic.warcraftlogs.com/v1/report/tables/%s/%s?end=36000000&by=target&abilityid=%s&encounter=%s&api_key=%s' % (table, reportId, ability, encounter, secrets.warcraft_logs_api_key)
+        table_url = 'https://classic.warcraftlogs.com/v1/report/tables/%s/%s?end=36000000&abilityid=%s&filter=%s&api_key=%s' % (table, reportId, ability, expression, secrets.warcraft_logs_api_key)
         # print(table_url)
         r = requests.get(table_url)
         r_json = r.json()
         total_time = r_json['totalTime']
-        for player in r_json['auras']:
+        for player in r_json['entries']:
           try:
-            no_casts = player['totalUses']
+            no_casts = player['total']
             uptime = player['uptime']
           except:
             # print('No uptime for ' + player['name'] + ' - ' + ability)
@@ -68,7 +68,8 @@ def main():
   abilities = ['11597', '12328', '24427', '25891']
   start_info = get_casts_type(reports, 'casts', encounters, abilities, 'Warrior')
   abilities = ['11597']
-  cast_info = get_debuffs(reports, 'debuffs', encounters, abilities, start_info)
+  expression = 'type%3D%22cast%22%20AND%20ability.id%3D11597%20AND%20NOT%20IN%20RANGE%20FROM%20type%3D%22applydebuffstack%22%20AND%20ability.id%3D11597%20AND%20stack%3D5%20TO%20type%3D%22removedebuff%22%20AND%20ability.id%3D11597%20GROUP%20BY%20target%20ON%20target%20END'
+  cast_info = get_effective(reports, 'casts', encounters, abilities, expression, start_info)
   print('Cast info retrieved')
 
   wks = wb.worksheet('warrior')
