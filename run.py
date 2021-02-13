63e6d0dd23d39cf00a34b35ebab34cff3e7c8794
Jazminite <jazminitehs@gmail.com>
2021-02-12 21:04:37 -0500

    add run.py

---
+++ run.py
@@ -0,0 +1,49 @@
+import time
+
+import buffs        # Buffs
+import caster       # Caster
+import damage       # Damage
+import deaths       # Deaths
+import eng          # Engineering damage
+import mage         # Mages
+import paladin      # Paladins
+import priest       # Priests
+import rogue        # Rogues
+import warlock      # Warlocks
+import warrior      # Warriors
+
+if __name__ == '__main__':
+    print('Getting Buffs...')
+    buffs.main()
+    time.sleep(15)
+    print('Getting Caster...')
+    caster.main()
+    time.sleep(15)
+    print('Getting Damage...')
+    damage.main()
+    time.sleep(15)
+    print('Getting Deaths...')
+    deaths.main()
+    time.sleep(15)
+    print('Getting Eng...')
+    eng.main()
+    time.sleep(15)
+    print('Getting Mage...')
+    mage.main()
+    time.sleep(15)
+    print('Getting Paladin...')
+    paladin.main()
+    time.sleep(15)
+    print('Getting Priest...')
+    priest.main()
+    time.sleep(15)
+    print('Getting Rogue...')
+    rogue.main()
+    time.sleep(15)
+    print('Getting Warlock...')
+    warlock.main()
+    time.sleep(15)
+    print('Getting Warrior...')
+    warrior.main()
+    print('ALL DONE!!!')
+    
